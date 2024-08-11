f352ad8183e339c78db9ee827f58d03b56f0ee3f
FGsfeajerbFsfwdkotevkfcjod <hookmehost+1@gmail.com>
2024-08-11 23:20:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv('7235429024:AAG8gjVNTCi-PqoSwdKzi7d5xOIEbtEILIU')
+CHANNEL_ID = os.getenv('-1002227671771')
+PHONEPE_EMAIL = os.getenv('xwatchmen1@gmail.com')
+PHONEPE_PASSWORD = os.getenv('Hsjejejjej#82')
